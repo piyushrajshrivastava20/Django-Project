bece2287c3d0a0419decaf9878620c837c85a585
piyushrajshrivastava20 <32167941+piyushrajshrivastava20@users.noreply.github.com>
2019-06-10 16:23:16 +0530

    Add files via upload

---
+++ pages/views.py
@@ -0,0 +1,15 @@
+
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home_view(request,*args, **kwargs):
+	# return HttpResponse("<h1>Hello World</h1>")
+
+	return render(request,"basic.html", {})
+
+
+
